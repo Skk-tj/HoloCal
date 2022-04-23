e2e72761f7bea9996f7dfbd113b40f9d1033d02c
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-04-23 12:34:58 -0700

    add animation to live icon

--- holo-wtf/Subviews/iPadOS/LivePaneView.swift
+++ holo-wtf/Subviews/iPadOS/LivePaneView.swift
@@ -12,6 +12,7 @@ struct LivePaneView: View {
     let live: LiveVideo
     
     @AppStorage("favouritedChannel") var favourited = Favourited()
+    @State var liveIconOpcality: Double = 0
     
     var body: some View {
         ZStack(alignment: .topLeading) {
@@ -64,7 +65,16 @@ struct LivePaneView: View {
             
             HStack(alignment: .top) {
                 VStack(alignment: .leading) {
-                    Text("LIVE")
+                    HStack(alignment: .top, spacing: 2) {
+                        Image(systemName: "circle.fill")
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                                    liveIconOpcality = 1
+                                }
+                            }
+                            .opacity(liveIconOpcality)
+                        Text("LIVE")
+                    }
                         .padding(5)
                         .background(.thickMaterial, in: RoundedRectangle(cornerRadius: 5, style: .continuous))
                         .font(.caption)
