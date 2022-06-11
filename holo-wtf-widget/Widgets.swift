08624e2db642c5f7cca55fac5d60f0aed5ef9ca3
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-06-10 22:04:20 -0700

    Refactoring

---
+++ holo-wtf-widget/Widgets.swift
@@ -0,0 +1,49 @@
+//
+//  Widgets.swift
+//  holo-wtf-widgetExtension
+//
+//
+//
+
+import WidgetKit
+import SwiftUI
+
+struct SmallLiveWidget: Widget {
+    let kind: String = "SmallLiveWidget"
+    
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: LiveWidgetProvider()) { entry in
+            SmallLiveWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("CURRENTLY_LIVE_WIDGET_DISPLAY_NAME")
+        .description("CURRENTLY_LIVE_WIDGET_DESCRIPTION")
+        .supportedFamilies([.systemSmall])
+    }
+}
+
+struct LivePaneWidget: Widget {
+    let kind: String = "LivePaneWidget"
+    
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: LiveWidgetProvider()) { entry in
+            LivePaneWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("CURRENTLY_LIVE_WIDGET_DISPLAY_NAME")
+        .description("CURRENTLY_LIVE_WIDGET_DESCRIPTION")
+        .supportedFamilies([.systemLarge])
+    }
+}
+
+struct MultipleLiveWidget: Widget {
+    let kind: String = "MultipleLiveWidget"
+    
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: MultipleLiveWidgetProvider()) { entry in
+            MultipleLiveWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("MULTIPLE_CURRENTLY_LIVE_WIDGET_DISPLAY_NAME")
+        .description("MULTIPLE_CURRENTLY_LIVE_WIDGET_DESCRIPTION")
+        .supportedFamilies([.systemMedium])
+    }
+}
+
