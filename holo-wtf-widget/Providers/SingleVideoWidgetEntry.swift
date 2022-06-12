744aaf10c5ea022e8b361780f2f048dd0c835b7a
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-06-12 00:43:15 -0700

    Refactoring, add upcoming provider

---
+++ holo-wtf-widget/Providers/SingleVideoWidgetEntry.swift
@@ -0,0 +1,18 @@
+//
+//  SingleVideoEntry.swift
+//  holo-wtf-widgetExtension
+//
+//  Created by Haoyi An on 2022-06-10.
+//
+
+import Foundation
+
+struct SingleVideoWidgetEntry: VideoTimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+    let video: LiveVideo?
+    let status: WidgetDataStatus
+    
+    let avatarData: Data
+    let thumbnailData: Data
+}
---
+++ holo-wtf-widget/Providers/UpcomingWidgetProvider.swift
@@ -0,0 +1,83 @@
+//
+//  UpcomingWidgetProvider.swift
+//  holo-wtf-widgetExtension
+//
+//  Created by Haoyi An on 2022-06-10.
+//
+
+import WidgetKit
+import Intents
+
+typealias UpcomingWidgetEntry = SingleVideoWidgetEntry
+
+struct UpcomingWidgetProvider: IntentTimelineProvider {    
+    typealias Entry = UpcomingWidgetEntry
+    
+    func placeholder(in context: Context) -> UpcomingWidgetEntry {
+        let channel: Channel = .init(id: "UCp6993wxpyDPHUpavwDFqgg", name: "Tokino Sora", photo: nil, org: "Hololive")
+        
+        let video: LiveVideo = .init(id: "sampleId", title: "My Debut Stream!", topicId: "debut", startScheduled: Date(), startActual: Date(), liveViewers: 1000, channel: channel)
+        
+        return UpcomingWidgetEntry(date: Date(), configuration: ConfigurationIntent(), video: video, status: .ok, avatarData: Data(), thumbnailData: Data())
+    }
+    
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (UpcomingWidgetEntry) -> Void) {
+        Task {
+            do {
+                var lives = try await VideoFetchService.shared.getVideos(from: "https://holodex.net/api/v2/live?org=Hololive&status=upcoming&type=stream")
+                
+                lives.sort(by: {$0.startActual ?? Date.distantFuture < $1.startActual ?? Date.distantFuture})
+                
+                let firstVideo = lives[0]
+                
+                let (avatarData, _) = try await URLSession.shared.data(from: firstVideo.channel.photo!)
+                let (thumbnailData, _) = try await URLSession.shared.data(from: URL(string: "https://i.ytimg.com/vi/\(firstVideo.id)/maxresdefault.jpg")!)
+                
+                let entry = UpcomingWidgetEntry(date: .now, configuration: configuration, video: lives[0], status: .ok, avatarData: avatarData, thumbnailData: thumbnailData)
+                completion(entry)
+            } catch {
+                let entry = UpcomingWidgetEntry(date: .now, configuration: configuration, video: nil, status: .network, avatarData: Data(), thumbnailData: Data())
+                
+                completion(entry)
+            }
+        }
+    }
+    
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<UpcomingWidgetEntry>) -> Void) {
+        Task {
+            do {
+                var lives = try await VideoFetchService.shared.getVideos(from: "https://holodex.net/api/v2/live?org=Hololive&status=upcoming&type=stream")
+                
+                lives.sort(by: {$0.startActual ?? Date.distantFuture < $1.startActual ?? Date.distantFuture})
+                
+                if lives.isEmpty {
+                    let entry = UpcomingWidgetEntry(date: .now, configuration: configuration, video: nil, status: .noVideo, avatarData: Data(), thumbnailData: Data())
+                    
+                    let entries: [UpcomingWidgetEntry] = [entry]
+                    
+                    let timeline = Timeline(entries: entries, policy: .atEnd)
+                    completion(timeline)
+                } else {
+                    let firstVideo = lives[0]
+                    
+                    let (avatarData, _) = try await URLSession.shared.data(from: firstVideo.channel.photo!)
+                    let (thumbnailData, _) = try await URLSession.shared.data(from: URL(string: "https://i.ytimg.com/vi/\(firstVideo.id)/maxresdefault.jpg")!)
+                    
+                    let entry = UpcomingWidgetEntry(date: .now, configuration: configuration, video: lives[0], status: .ok, avatarData: avatarData, thumbnailData: thumbnailData)
+                    
+                    let entries: [UpcomingWidgetEntry] = [entry]
+                    
+                    let timeline = Timeline(entries: entries, policy: .atEnd)
+                    completion(timeline)
+                }
+            } catch {
+                let entry = UpcomingWidgetEntry(date: .now, configuration: configuration, video: nil, status: .network, avatarData: Data(), thumbnailData: Data())
+                
+                let entries: [UpcomingWidgetEntry] = [entry]
+                
+                let timeline = Timeline(entries: entries, policy: .atEnd)
+                completion(timeline)
+            }
+        }
+    }
+}
