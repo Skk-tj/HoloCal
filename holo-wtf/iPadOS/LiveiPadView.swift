c0aa377d829c1140e507a0d8df93f228ff398c9e
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-07-01 16:38:24 -0700

    iPad code refactoring

--- holo-wtf/iPadOS/LiveiPadView.swift
+++ holo-wtf/iPadOS/LiveiPadView.swift
@@ -10,85 +10,19 @@ import SwiftUI
 struct LiveiPadView: View {
     @StateObject var live: LiveViewModel
     
-    @AppStorage("favouritedChannel") var favourited = Favourited()
-    @AppStorage("generationListSelection") var generationListSelection = Set(GenerationEnum.allCases)
-    
-    @State var searchText: String = ""
-    
-    @State var sortingSelection: SortingStrategy? = nil
     @State var isSorting: Bool = false
-    
-    let layout = [
-        GridItem(.adaptive(minimum: 300), spacing: 10)
-    ]
+    @State var sortingSelection: SortingStrategy? = nil
     
     init() {
         self._live = StateObject(wrappedValue: LiveViewModel())
     }
     
     var body: some View {
-        ScrollView {
-            if (live.dataStatus == .working) {
-                ProgressView()
-            } else if (live.dataStatus == .fail) {
-                Spacer()
-                Label("FAILED_TO_RETRIEVE_NEW_DATA", systemImage: "exclamationmark.circle.fill")
-                    .foregroundColor(.secondary)
-                Spacer()
-            } else {
-                LazyVGrid(columns: layout, spacing: 50) {
-                    if searchText.isEmpty {
-                        //- MARK: Favourites Section
-                        FavouritedForEachView(cellView: { live in
-                            LinkedVideoView(url: live.url) {
-                                LivePaneView(live: live)
-                            }
-                            .contextMenu {
-                                VideoContextMenu(video: live)
-                            }
-                        })
-                        
-                        //- MARK: Not favourited
-                        NotFavouritedForEachView(cellView: { live in
-                            LinkedVideoView(url: live.url) {
-                                LivePaneView(live: live)
-                            }
-                            .contextMenu {
-                                VideoContextMenu(video: live)
-                            }
-                        })
-                    } else {
-                        SearchForEachView(searchText: searchText, cellView: { live in
-                            LinkedVideoView(url: live.url) {
-                                LivePaneView(live: live)
-                            }
-                            .contextMenu {
-                                VideoContextMenu(video: live)
-                            }
-                        })
-                    }
-                }
-                .padding(30)
-                
-                Divider()
-                    .padding(.horizontal)
-                
-                let filteredVideoCount = live.videoList.filter { video in
-                    !generationListSelection.contains(video.channel.talent.inGeneration)
-                }
-                    .count
-                
-                if filteredVideoCount == 0 {
-                    Text("LIVE_VIEW_CURRENT_COUNT \(live.videoList.count)")
-                        .foregroundColor(.secondary)
-                        .padding(.bottom, 30)
-                } else {
-                    Text("LIVE_VIEW_CURRENT_COUNT \(live.videoList.count) \(filteredVideoCount)")
-                        .foregroundColor(.secondary)
-                        .padding(.bottom, 30)
-                }
-            }
-        }
+        iPadLazyGirdView(singleVideoView: { live in
+            LivePaneView(live: live)
+        }, countView: {
+            LiveCountView()
+        })
         .environmentObject(live as VideoViewModel)
         .task {
             await live.getLive()
@@ -98,13 +32,6 @@ struct LiveiPadView: View {
             sortingSelection = nil
         }
         .navigationTitle("LIVE_VIEW_TITLE")
-        .searchable(text: $searchText, prompt: "SEARCH_BY_NAME_OR_TAG") {
-            if searchText.isEmpty {
-                ForEach(live.getSearchSuggestions(), id: \.self) { suggestion in
-                    Text("\(suggestion)").searchCompletion(suggestion)
-                }
-            }
-        }
         .toolbar {
             ToolbarItemGroup {
                 LiveViewToolbar(sortingSelection: $sortingSelection, isSorting: $isSorting)
---
+++ holo-wtf/iPadOS/iPadLazyGirdView.swift
@@ -0,0 +1,80 @@
+//
+//  iPadLazyGirdView.swift
+//  holo-wtf
+//
+//
+//
+
+import SwiftUI
+
+struct iPadLazyGirdView<VideoContent: View, CountContent: View>: View {
+    @EnvironmentObject var videoViewModel: VideoViewModel
+    
+    @AppStorage("favouritedChannel") var favourited = Favourited()
+    @AppStorage("generationListSelection") var generationListSelection = Set(GenerationEnum.allCases)
+    
+    @State var searchText: String = ""
+    
+    @ViewBuilder let singleVideoView: (_ live: LiveVideo) -> VideoContent
+    @ViewBuilder let countView: () -> CountContent
+    
+    let layout = [
+        GridItem(.adaptive(minimum: 300), spacing: 10)
+    ]
+    
+    var body: some View {
+        ScrollView {
+            switch videoViewModel.dataStatus {
+            case .working:
+                ProgressView()
+            case .fail:
+                Spacer()
+                Label("FAILED_TO_RETRIEVE_NEW_DATA", systemImage: "exclamationmark.circle.fill")
+                    .foregroundColor(.secondary)
+                Spacer()
+            case .success:
+                LazyVGrid(columns: layout, spacing: 50) {
+                    if searchText.isEmpty {
+                        if videoViewModel.videoList.filter { video in favourited.contains(where: { video.channel.id == $0 })}.count != 0 && videoViewModel.dataStatus == .success {
+                            FavouritedForEachView(cellView: { live in
+                                singleVideoView(live)
+                            })
+                        }
+                        
+                        NotFavouritedForEachView(cellView: { live in
+                            singleVideoView(live)
+                        })
+                    } else {
+                        SearchForEachView(searchText: searchText, cellView: { live in
+                            LinkedVideoView(url: live.url) {
+                                singleVideoView(live)
+                            }
+                            .contextMenu {
+                                VideoContextMenu(video: live)
+                            }
+                        })
+                    }
+                }
+                .padding(30)
+                
+                Divider()
+                    .padding(.horizontal)
+                
+                countView()
+            }
+        }
+        .searchable(text: $searchText, prompt: "SEARCH_BY_NAME_OR_TAG") {
+            if searchText.isEmpty {
+                ForEach(videoViewModel.getSearchSuggestions(), id: \.self) { suggestion in
+                    Text("\(suggestion)").searchCompletion(suggestion)
+                }
+            }
+        }
+    }
+}
+
+//struct iPadLazyGirdView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        iPadLazyGirdView()
+//    }
+//}
