fceb60fbd7c9cc944bc55d885251762a7db44a85
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-10-06 22:10:28 -0700

    Refactor

---
+++ holo-wtf/Models/Organization.swift
@@ -0,0 +1,13 @@
+//
+//  Organization.swift
+//  holo-wtf
+//
+//
+//
+
+import Foundation
+
+enum Organization: String {
+    case hololive = "Hololive"
+    case nijisanji = "Nijisanji"
+}
--- holo-wtf/ViewModel/UpcomingViewModel.swift
+++ holo-wtf/ViewModel/UpcomingViewModel.swift
@@ -15,10 +15,8 @@ class UpcomingViewModel: VideoViewModel {
         
         await getVideo(url: "https://holodex.net/api/v2/live?status=upcoming&type=stream&org=Hololive&include=songs,mentions&max_upcoming_hours=\(upcomingLookAhead)") { responseResult in
             self.videoList = responseResult
-            self.videoList.sort(by: {$0.startScheduled ?? Date.distantFuture < $1.startScheduled ?? Date.distantFuture})
-            self.videoList = self.videoList.filter { video in
-                video.channel.org == "Hololive"
-            }
+            self.videoList.sort(by: upcomingSortStrategy)
+            self.videoList = self.videoList.filter {$0.isHololive}
         }
     }
 }
