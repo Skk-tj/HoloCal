1d0b3be3e1334a796290f76e4c56a22d1a7df8df
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-10-11 21:53:55 -0700

    Add nijisanji data

---
+++ holo-wtf/Models/Nijisanji.swift
@@ -0,0 +1,12 @@
+//
+//  Nijisanji.swift
+//  holo-wtf
+//
+//  Created by Haoyi An on 2022-10-11.
+//
+
+import Foundation
+
+enum nijisanjiTalents: String, Codable {
+    case moira = "UCvmppcdYf4HOv-tFQhHHJMA"
+}
