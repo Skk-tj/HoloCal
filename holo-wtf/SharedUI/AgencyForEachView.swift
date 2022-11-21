68d7d15e70ef8a808fe799b01b1ee2914ac25692
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2022-11-20 22:10:50 -0800

    video list refactor

---
+++ holo-wtf/SharedUI/AgencyForEachView.swift
@@ -0,0 +1,34 @@
+//
+//  AgencyForEachView.swift
+//  holo-wtf
+//
+//  Created by Haoyi An on 2022-11-20.
+//
+
+import SwiftUI
+
+struct AgencyForEachView<Content: View>: View {
+    @ViewBuilder let singleAgency: (_ agency: AgencyEnum) -> Content
+    
+    @ViewBuilder
+    var body: some View {
+        ForEach(AgencyEnum.allCases, id: \.self) { agency in
+            singleAgency(agency)
+        }
+    }
+}
+
+struct AgencyForEachView_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            AgencyForEachView(singleAgency: { agency in
+                VStack {
+                    Text(agencyEnumToAgency[agency]!.localizedName)
+                    Text(agencyEnumToAgency[agency]!.altLocalizedName)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            })
+        }
+    }
+}
