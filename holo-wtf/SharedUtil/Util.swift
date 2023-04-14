24d43e85d1eddd97c8b03b9322b02cf897271193
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2023-04-13 20:12:27 -0700

    fix decoder

--- holo-wtf/SharedUtil/Util.swift
+++ holo-wtf/SharedUtil/Util.swift
@@ -52,7 +52,8 @@ func getLiveVideoJSONDecoder() -> JSONDecoder {
     
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSX"
-    dateFormatter.calendar = Calendar(identifier: .gregorian)
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
     
     decoder.dateDecodingStrategy = .formatted(dateFormatter)
     
